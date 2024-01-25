4725af83f175ced0168bb3db60bb94e5a6b37cef
Brett Jones <contact.waycoolapps@gmail.com>
2024-01-25 14:56:49 -0600

    Add missing StorageProvider files

---
+++ nolimitgui/CommonSrc/QtSource/StorageProvider.cpp
@@ -0,0 +1,147 @@
+//============================================================================
+// Copyright (C) 2024 Brett R. Jones
+//
+// Code copyrighted by Brett R. Jones is under dual license similar to Ruby's license
+// See file COPYING and LEGAL in root of the No Limit Connect project
+//
+// bjones.engineer@gmail.com
+// https://nolimitconnect.com
+//============================================================================
+
+#include "StorageProvider.h"
+
+#include "AppCommon.h"
+#include "GuiFileXferMgr.h"
+#include "GuiHelpers.h"
+#include "StorageFile.h"
+
+#include <P2PEngine/P2PEngine.h>
+
+#include <QDir>
+
+#if defined (Q_OS_ANDROID)
+#include <QtQml/QQmlFile>
+
+# if QT_VERSION < QT_VERSION_CHECK(6,0,0)
+#  include <QtAndroid>
+# else
+#  include <QtCore/private/qandroidextras_p.h>
+# endif
+#endif //defined (Q_OS_ANDROID)
+
+
+//============================================================================
+StorageProvider& GetStorageProvider( void )
+{
+    static StorageProvider g_StorageProvider;
+    return g_StorageProvider;
+}
+
+//============================================================================
+void StorageProvider::fromGuiBrowseFiles( std::string& folderName, uint8_t fileFilterMask )
+{
+#if !defined(TARGET_OS_ANDROID)
+    GetPtoPEngine().getFromGuiInterface().fromGuiBrowseFiles( folderName, fileFilterMask );
+    return;
+#endif // !defined(TARGET_OS_ANDROID)
+
+    std::string testFile;
+
+	std::vector<FileInfo> fileList;
+	if( 0 == fileFilterMask )
+	{
+		fileFilterMask = VXFILE_TYPE_ALLNOTEXE | VXFILE_TYPE_DIRECTORY;
+	}
+
+	VxGUID onlineId = GetAppInstance().getMyOnlineId();
+    QDir browseDir( folderName.c_str() );
+
+    QFileInfoList fileInfoList = browseDir.entryInfoList();
+    LogMsg( LOG_VERBOSE, "%d files in dir %s", fileList.size(), folderName.c_str() );
+    for( auto fileListInfo : fileInfoList )
+    {
+        std::string fileName = fileListInfo.filePath().toUtf8().constData();
+
+		if( fileListInfo.isDir() )
+		{
+			LogMsg( LOG_VERBOSE, "Directory %s", fileName.c_str() );
+
+			if( fileFilterMask & VXFILE_TYPE_DIRECTORY )
+			{
+				VxFileUtil::assureTrailingDirectorySlash( fileName );
+				FileInfo dirInfo( onlineId, fileName, 0, VXFILE_TYPE_DIRECTORY );
+				GetAppInstance().getFileXferMgr().toGuiFileList( dirInfo );
+			}
+		}
+		else if( fileListInfo.isExecutable() )
+		{
+			LogMsg( LOG_VERBOSE, "Executable ignored File %s", fileName.c_str() );
+		}
+		else if( fileListInfo.isReadable() )
+		{
+            int64_t fileLen{ 0 };
+            std::string resolvedName;
+#if defined(TARGET_OS_ANDROID)
+            QString contentPath( fileName.c_str() );
+            QUrl contentUrl(contentPath);
+            QString fileNameResolved = QQmlFile::urlToLocalFileOrQrc(contentUrl);
+            resolvedName = fileNameResolved.toStdString();
+#endif // defined(TARGET_OS_ANDROID)
+
+            //if( StorageFile::resolveFileNameAndLength( fileName, resolvedName, fileLen ) )
+            if( !resolvedName.empty() )
+            {
+                LogMsg( LOG_VERBOSE, "Readable File len1 %" PRId64 " resolved to %s from %s",
+                       fileLen, resolvedName.c_str(), fileName.c_str());
+
+			    uint8_t fileType = VxFileNameToFileType( fileName );
+                if( fileLen && fileType & fileFilterMask )
+			    {
+                    LogMsg( LOG_VERBOSE, "Readable File %s len %" PRId64 " type 0x%x ", resolvedName.c_str(), fileLen, fileType );
+                    std::size_t found = resolvedName.find( "Test.x264-ION10.mp4" );
+                    if( found != std::string::npos )
+                    {
+                        testFile = resolvedName;
+                    }
+
+				    FileInfo fileInfo( onlineId, resolvedName, fileLen, fileType );
+				    fileInfo.setIsInLibrary( GetPtoPEngine().fromGuiGetIsFileInLibrary( resolvedName ) );
+				    fileInfo.setIsSharedFile( GetPtoPEngine().fromGuiGetIsFileShared( fileInfo ) );
+
+				    // GetAppInstance().getFileXferMgr().toGuiFileList( fileInfo );
+			    }
+            }
+            else
+            {
+                LogMsg( LOG_VERBOSE, "Could Not Resolve length %" PRId64 " File %s", fileLen, fileName.c_str() );
+            }
+		}
+		else
+		{
+			LogMsg( LOG_VERBOSE, "NOT Readable File %s", fileName.c_str() );
+		}
+    }
+
+	GetAppInstance().getFileXferMgr().toGuiFileListCompleted();
+}
+
+//============================================================================
+void StorageProvider::loadUrl( const QUrl &url )
+{
+/*
+    m_sequence.clear();
+    qDebug() << "hello" << url << QQmlFile::urlToLocalFileOrQrc( url );
+    QFile file(QQmlFile::urlToLocalFileOrQrc(url));
+    if (!file.open(QFile::ReadOnly))
+    {
+        emit error(tr("Could not open file. '%1'").arg(file.errorString()));
+        return;
+    }
+*/
+}
+
+//============================================================================
+bool StorageProvider::requestAndroidStoragePermissions( void )
+{
+    return GuiHelpers::checkUserPermission({ "android.permission.READ_EXTERNAL_STORAGE"});
+}
---
+++ nolimitgui/CommonSrc/QtSource/StorageProvider.h
@@ -0,0 +1,41 @@
+#pragma once
+//============================================================================
+// Copyright (C) 2024 Brett R. Jones
+//
+// Code copyrighted by Brett R. Jones is under dual license similar to Ruby's license
+// See file COPYING and LEGAL in root of the No Limit Connect project
+//
+// bjones.engineer@gmail.com
+// https://nolimitconnect.com
+//============================================================================
+
+#if defined(TARGET_OS_LINUX)
+#include <QWidget> // must be declared first or linux Qt will error in qmetatype.h 2167:23: array subscript value 53 is outside the bounds
+#endif // defined(TARGET_OS_LINUX)
+
+#include <string>
+#include <CoreLib/VxFileTypeMasks.h>
+
+// Android 11 (API level 30) implemented scoped storage which is why this class exists
+
+class QUrl;
+
+class StorageProvider
+{
+
+public:
+    StorageProvider() = default;
+    StorageProvider( const StorageProvider& rhs ) = delete;
+	virtual ~StorageProvider() = default;
+
+
+    void				fromGuiBrowseFiles( std::string& folderName, uint8_t fileFilterMask = VXFILE_TYPE_ALLNOTEXE | VXFILE_TYPE_DIRECTORY );
+
+protected:
+    bool                requestAndroidStoragePermissions( void );
+
+    void                loadUrl( const QUrl &url );
+
+};
+
+StorageProvider& GetStorageProvider( void );
