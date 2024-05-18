de146ed09e9da127c224c50d2ed07672ff2edf46
nolimit <nolimit@nolimitconnect.nolimitconnect.net>
2024-05-18 08:58:25 -0500

    Wrong App ID in flatpak metainfo

--- nolimitlib/DependLibs/CoreLib/AppVersion.h
+++ nolimitlib/DependLibs/CoreLib/AppVersion.h
@@ -6,14 +6,14 @@
 #define APP_DOMAIN_NAME			"nolimitconnect"
 #define APP_PACKAGE				"com.nolimitconnect.NoLimitConnect"
 
-#define APP_VERSION				"1.0.7"
+#define APP_VERSION				"1.0.8"
 #define APP_MAJOR_VERSION		1
 #define APP_MINOR_VERSION		0
-#define APP_PATCH_VERSION		7
+#define APP_PATCH_VERSION		8
 /* Version number: (major<<16) + (minor<<8) + patch */
 #define APP_VERSION_BINARY		((APP_MAJOR_VERSION<<16) + (APP_MINOR_VERSION<<8) + APP_PATCH_VERSION)
 
-#define PACKAGE_STRING			"NoLimitConnect 1.0.7"
+#define PACKAGE_STRING			"NoLimitConnect 1.0.8"
 
 /* redefine for different library needs */
 #define PACKAGE_BUGREPORT		APP_URL
