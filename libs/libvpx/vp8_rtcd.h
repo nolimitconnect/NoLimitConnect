f106eec7af11b4053e12cd4976c6b85397f23327
Brett Jones <bjones.engineer@gmail.com>
2025-08-29 14:43:47 -0500

    Fix windows build after move of libraries, Add missing files that were not checked in after the move of libraries

---
+++ libs/libvpx/vp8_rtcd.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// rtcd is only used for intel processors
+#if defined(TARGET_OS_LINUX)
+# if defined(TARGET_CPU_X86_64)
+#  include "vp8_rtcd_linux_x64.h"
+# endif // defined(TARGET_CPU_X86_64)
+#endif // defined(TARGET_OS_LINUX)
+
+#if defined(TARGET_OS_WINDOWS)
+# include "vp8_rtcd_win_x64.h"
+#endif // defined(TARGET_OS_ANDROID)
+
+#if defined(TARGET_CPU_ARM64) || defined(TARGET_CPU_ARM32)
+# include "vp8_rtcd_arm64.h"
+#endif // defined(TARGET_CPU_ARM64) || defined(TARGET_CPU_ARM32)
---
+++ libs/libvpx/vpx_dsp_rtcd.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// rtcd is only used for intel processors
+#if defined(TARGET_OS_LINUX)
+# if defined(TARGET_CPU_X86_64)
+#  include "vpx_dsp_rtcd_linux_x64.h"
+# endif // defined(TARGET_CPU_X86_64)
+#endif // defined(TARGET_OS_LINUX)
+
+#if defined(TARGET_OS_WINDOWS)
+# include "vpx_dsp_rtcd_win_x64.h"
+#endif // defined(TARGET_OS_WINDOWS)
+
+#if defined(TARGET_CPU_ARM64) || defined(TARGET_CPU_ARM32)
+# include "vpx_dsp_rtcd_arm64.h"
+#endif // defined(TARGET_CPU_ARM64) || defined(TARGET_CPU_ARM32)
